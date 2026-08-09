6c5ffbf0c981d847e4cbb1edb4a65cda744dcaec
Heather813903 <heathersmith9378@gmail.com>
2026-08-09 18:02:10 -0400

    Add validation to todo updates

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import TextInputWithLabel from '../../shared/TextInputWithLabel.jsx';
+import { isValidTodoTitle } from '../../utils/todoValidation.js'; 
 
 function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
   const [isEditing, setIsEditing] = useState(false);
@@ -28,7 +29,11 @@ function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
         <button type="button" onClick={handleCancel}>
           Cancel
         </button>
-        <button type="button" onClick={handleUpdate}>
+        <button 
+        type="button"
+         onClick={handleUpdate}
+         disabled={!isValidTodoTitle(workingTitle)}
+         >
   Update
 </button>
       </>
