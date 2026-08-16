3e4a4c4d79f8436bf7cdbba6ed61f5181862fce3
Heather813903 <heathersmith9378@gmail.com>
2026-08-16 17:08:45 -0400

    Add user authenticationhttps://smcr.browse-it.net/?v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./shared/Header.jsx";
 import TodosPage from "./features/Todos/TodosPage.jsx";
+import Logon from "./features/Logon.jsx";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+
   return (
     <div>
-      <Header />
-      <TodosPage />
+      <Header token={token} onSetToken={setToken} onSetEmail={setEmail} />
+
+      {token ? (
+        <TodosPage token={token} />
+      ) : (
+        <Logon onSetEmail={setEmail} onSetToken={setToken} />
+      )}
     </div>
   );
 }
