8fe2d2143a0819b8a796a96f47efb17b3a5c92f2
Heather813903 <heathersmith9378@gmail.com>
2026-08-17 16:11:38 -0400

    Fetch todos from API

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoForm from "./TodoForm.jsx";
 import TodoList from "./TodoList/TodoList.jsx";
 
-function TodosPage() {
+function TodosPage({ token }) {
   const [todoList, setTodoList] = useState([]);
+  const [error, setError] = useState("");
+  const [isTodoListLoading, setIsTodoListLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        setIsTodoListLoading(true);
+
+        const params = new URLSearchParams({
+          limit: 100,
+        });
+
+        const response = await fetch(`/api/tasks?${params}`, {
+          headers: {
+            "X-CSRF-TOKEN": token,
+          },
+          credentials: "include",
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+          setTodoList(data.tasks);
+        } else if (response.status === 401) {
+          throw new Error("unauthorized");
+        } else {
+          throw new Error("Failed to fetch todos");
+        }
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsTodoListLoading(false);
+      }
+    };
+    if (token) {
+      fetchTodos();
+    }
+  }, [token]);
 
   function addTodo(todoTitle) {
     const newTodo = {
