0ee94227a51bcdc921b22cbc6585039b98ed849c
Heather813903 <heathersmith9378@gmail.com>
2026-09-08 15:49:30 -0400

    Improve 404 navigation

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -6,7 +6,13 @@ function NotFoundPage() {
       <h2>404 - Page Not Found</h2>
       <p>The page you are looking for does not exist.</p>
 
-      <Link to="/">Go Home</Link>
+      <p>
+        <Link to="/">Go Home</Link>
+      </p>
+
+      <p>
+        <Link to="/about">Go to About</Link>
+      </p>
     </div>
   );
 }
