5247514cbff7532e5fa75bb67eeef4bd7eeef772
Heather813903 <heathersmith9378@gmail.com>
2026-09-09 20:51:10 -0400

    Style shared controls and app pages

--- src/shared/FilterInput.jsx
+++ src/shared/FilterInput.jsx
@@ -1,8 +1,13 @@
+import styles from "./FormControls.module.css";
+
 function FilterInput({ filterTerm, onFilterChange }) {
   return (
-    <div>
-      <label htmlFor="filterInput">Search todos:</label>
+    <div className={styles.control}>
+      <label className={styles.label} htmlFor="filterInput">
+        Search todos:
+      </label>
       <input
+        className={styles.input}
         id="filterInput"
         type="text"
         value={filterTerm}
